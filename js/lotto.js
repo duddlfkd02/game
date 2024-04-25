752ca183a53109c320642e47d77c816e71a682a3
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-04-25 14:47:20 +0900

    ball color

--- js/lotto.js
+++ js/lotto.js
@@ -6,15 +6,41 @@ function drawBall(number, $parent){
     const $ball = document.createElement('div');
         $ball.className = 'ball';
         $ball.textContent = number;
+        colorize(number, $ball);
         $parent.appendChild($ball);
 }
-//async, await 사용 시 Promise 변수 설정.
-const setTimeoutPromise = (ms) => new Promise((resolve, reject) =>{
-    setTimeout(resolve,ms);
-});
+//공 색깔 함수
+function colorize(number, $tag){
+    if(number < 10){
+        $tag.style.backgroundColor = 'tomato';
+        $tag.style.color = 'white';
+    } else if (number < 20){
+        $tag.style.backgroundColor = 'orange';
+        $tag.style.color = 'white';
+    } else if (number < 30){
+        $tag.style.backgroundColor = 'yellow';
+    } else if (number < 40){
+        $tag.style.backgroundColor = 'blue';
+        $tag.style.color = 'white';
+    } else {
+        $tag.style.backgroundColor = 'pink';
+    }
+}
 
+
+//* -- async, await 사용 시 Promise 변수 설정. --*
+// const setTimeoutPromise = (ms) => new Promise((resolve, reject) =>{
+//     setTimeout(resolve,ms);
+// });
+let clicked = true; //flag 함수 선언
 $form.addEventListener('submit',function(e){
     e.preventDefault();
+    if(!clicked){ // 이미 클릭하면 함수 종료
+        return;
+    }
+    clicked = false;
+    $result.innerHTML = '당첨숫자: ';
+    $bonus.innerHTML = '보너스 숫자: ';
     //검사
     const string = e.target.input.value; // 1,2,3,4,5,6 -> [1,2,3,4,5,6]
     if(!string.trim()){//trim은 공백을 줄여줌
@@ -58,16 +84,16 @@ $form.addEventListener('submit',function(e){
 
     //등수 체크하기
     let count = 0;
-    // myNum.forEach((my, function(){
+    myNum.forEach((function(my){
+        if(winBalls.includes(my)){
+            count++;  
+        }
+    }));
+    // myNum.forEach((my) => {
     //     if(winBalls.includes(my)){
     //         count++;
     //     }
-    // }));
-    myNum.forEach((my) => {
-        if(winBalls.includes(my)){
-            count++;
-        }
-    });
+    // });
     if(count === 6){
         alert('1등! 당첨입니다!')
     } else if(count === 5){
@@ -84,15 +110,19 @@ $form.addEventListener('submit',function(e){
         alert('다음 기회에')
     };
 
+    clicked = true; // 클릭 가능
+    
+});
 
-    //async 와 await으로 수정하기 (수정 시 위에 function(e)를 async(event)로 수정)
+    //* -- async 와 await으로 수정하기 (수정 시 위에 function(e)를 async(event)로 수정)
     // for(let i = 0; i < winBalls.length; i++){
     //     await setTimeoutPromise(1000);
     //     drawBall(winBalls[i], $result);
     // }
     // await setTimeoutPromise(1000);
     // drawBall(bonus, $bonus);
-});
+
+
     //* -- drawBall 함수 빼기 전 원래 코드 --*/
     // for(let i = 0; i < 6; i++){
     //     setTimeout (function(){
